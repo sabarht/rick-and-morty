1eb452ffcc38c786ba793f7c71227d6570ad8103
inbar <133640288+InbarGluska@users.noreply.github.com>
2023-06-15 11:48:36 +0200

    firstTask

--- components/card/card.js
+++ components/card/card.js
@@ -1,1 +1,26 @@
-export function createCharacterCard() {}
+export function createCharacterCard(characters) {
+  const newCard = document.createElement("ul");
+  newCard.classList.add("card-container");
+  newCard.innerHTML = `<li class="card">
+  <div class="card__image-container">
+    <img class="card__image" src=${characters.image} alt=" " />
+    <div class="card__image-gradient"></div>
+  </div>
+  <div class="card__content">
+    <h2 class="card__title">${characters.name}</h2>
+    <dl class="card__info">
+      <dt class="card__info-title">Status</dt>
+      <dd class="card__info-description">${characters.status}</dd>
+      <dt class="card__info-title">Type</dt>
+      <dd class="card__info-description">${characters.type}</dd>
+      <dt class="card__info-title">Occurrences</dt>
+      <dd class="card__info-description">${characters.episode.length}</dd>
+    </dl>
+  </div>
+</li>`;
+
+  const formContainer = document.getElementsByClassName("card-container");
+  formContainer.append(newCard);
+
+  //activateButtons();
+}
