cead19635f7ece3adeb09b0af4f7b2f3acc222a6
Saba <sabarht@gmail.com>
2023-06-15 17:00:00 +0200

    final v1

---
+++ components/nav-button/nav-button.js
@@ -0,0 +1,9 @@
+// import { nextButton } from "../../index.js";
+// export function activateButtons(info) {
+//   const a = document.createElement("a");
+//   a.setAttribute("src", info.next);
+//   nextButton.append(a);
+//   console.log(info);
+//   console.log(a);
+// }
+// activateButtons();
